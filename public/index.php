1b28a6820c6e1eecfff4b18d206bf9dc84ae0144
Virginia <virginia@ieessanvicente.com>
2023-10-14 21:50:58 +0200

    otra ve

--- public/index.php
+++ public/index.php
@@ -7,7 +7,7 @@ echo '<table>
     <title>Parámetros del servidor</title>
 </head>
 <body>
-<h1>Parámetros del servidor. 21:42</h2>
+<h1>Parámetros del servidor. 21:48</h2>
     <table border="1">
         <tr>
             <th>PARÁMETRO</th>
